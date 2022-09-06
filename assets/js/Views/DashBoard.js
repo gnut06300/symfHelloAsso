3fca33017bdd70d461e46f27405562df7b4eb7fd
yoan <yoan.borne@epitech.eu>
2022-09-06 17:15:28 +0200

    update projet

---
+++ assets/js/Views/DashBoard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function DashBoard() {
+  
+    return (
+
+            <div className="home">
+                <div className="container">    
+                    <h1 className="text-center mt-5 mb-3">DashBoard Page</h1>
+                </div>
+            </div>
+    );
+}
+   
+export default DashBoard;
