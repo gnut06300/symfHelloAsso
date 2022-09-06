52522ce7694eb4b700c8ea3278bedca596fe2e7c
Gerald Col <gnut@gnut.eu>
2022-09-06 14:07:11 +0200

    Webpack et React

---
+++ assets/js/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "../styles/app.css";
+
+
+export default function App() {
+    
+    return (
+        <div className="app">Hello React</div>
+    );
+}
+
+const rootElement = document.getElementById("app");
---
+++ assets/js/Components/data/menu.js
@@ -0,0 +1,29 @@
+const menu = [
+    {
+        title: "Accueil",
+        url: "/",
+        cName: "nav-links",   
+    },
+    {
+        title: "A propos",
+        url: "/about",
+        cName: "nav-links"
+    },
+    {
+        title: "Contact",
+        url: "/contact",
+        cName: "nav-links"
+    },
+    {
+        title: "DashBoard",
+        url: "/dashboard",
+        cName: "nav-links"
+    },
+    {
+        title: "Login",
+        url: "/login",
+        cName: "nav-links"
+    }
+    ];
+    
+export default menu;
